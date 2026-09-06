9edec6ddbad58c417fc2ee607ba76bd8c5cba1d6
kapilan <sriranjankapilan@gmail.com>
2026-09-06 10:36:51 +0530

    entry point created

---
+++ backend/weekly-report-backend/src/main/java/com/technical/task/weeklyreportbackend/WeeklyReportBackendApplication.java
@@ -0,0 +1,13 @@
+package com.technical.task.weeklyreportbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeeklyReportBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeeklyReportBackendApplication.class, args);
+    }
+
+}
